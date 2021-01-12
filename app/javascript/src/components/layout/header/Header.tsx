d5db4cc2cbdf7901e03deb1333fbe4c3da8c7d6f
Ari Firmanto <arifirmanto@gmail.com>
2021-01-13 00:27:46 +0700

    adding header component

--- app/javascript/src/components/layout/header/Header.tsx
+++ app/javascript/src/components/layout/header/Header.tsx
@@ -1,9 +1,24 @@
 import './header.css';
 import React from 'react';
+import {  Menu } from 'antd';
+import IconLogo from 'assets/images/ui-logo.svg';
 
 const Header = () => {
     return (
       <header className="ant-layout-header app-header">
+        <div style={{display: 'flex'}}>
+          <div className="logo" >
+            <img src={IconLogo} width="50" height="50"/>
+          </div>
+          <Menu className="nav-list" theme="light" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item className="nav-item" key="1">DASHBOARD</Menu.Item>
+            <Menu.Item className="nav-item" key="2">PACKETS</Menu.Item>
+            <Menu.Item className="nav-item" key="3">TEAM PACKETS</Menu.Item>
+            <Menu.Item className="nav-item" key="3">ADMIN</Menu.Item>
+            <Menu.Item className="nav-item" key="3">PROFILE</Menu.Item>
+          </Menu>
+        </div>
+
       </header>
     )
 }
